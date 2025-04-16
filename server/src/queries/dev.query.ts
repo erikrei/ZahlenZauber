a4713060ccd7d89fcc0e13350cf18530105e6636
Erik Reisswig <erikreisswig@gmx.de>
2025-04-16 14:28:06 +0200

    Email-Adressen müssen jetzt einzigartig sein

--- server/src/queries/dev.query.ts
+++ server/src/queries/dev.query.ts
@@ -1,9 +1,9 @@
 import { PoolConnection } from "mysql2/typings/mysql/lib/PoolConnection"
 import { connection } from "../config/db_connection"
 
-const truncateUsersTable = (): Promise<String> => {
+const truncateUsersTable = (): Promise<string> => {
     return new Promise((resolve, reject) => {
-        connection.getConnection((err, poolConnection: PoolConnection) => {
+        connection.getConnection((_, poolConnection: PoolConnection) => {
             poolConnection.query("TRUNCATE TABLE users;", ((err, result) => {
                 poolConnection.release();
 
@@ -14,6 +14,31 @@ const truncateUsersTable = (): Promise<String> => {
     })
 }
 
+const createUsersTable = (): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        connection.getConnection((_, poolConnection: PoolConnection) => {
+            poolConnection.query(
+                `
+                CREATE TABLE users (
+                    user_id SMALLINT UNSIGNED AUTO_INCREMENT,
+                    user_name VARCHAR(20) NOT NULL,
+                    user_email VARCHAR(40) NOT NULL,
+                    user_password VARCHAR(60) NOT NULL,
+                    CONSTRAINT pk_user PRIMARY KEY (user_id),
+                    CONSTRAINT uq_user_name UNIQUE (user_name),
+                    CONSTRAINT uq_user_email UNIQUE (user_email)
+                );  
+                `,
+                (err) => {
+                    if (err) return reject(err.code)
+                    return resolve("TABLE `zahlenzauber_db.users` wurde erfolgreich erstellt.")
+                }
+            )
+        })
+    })
+}
+
 export default {
-    truncateUsersTable
+    truncateUsersTable,
+    createUsersTable
 }
